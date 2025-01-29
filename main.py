511330db52bde3e5adc2a97e11cead090d4d2b76
Fscarmon <156925521+Fscarmon@users.noreply.github.com>
2025-01-30 04:44:58 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -131,7 +131,7 @@ def process_email(email, max_captcha_retries, max_email_retries, tg_token, tg_ch
                     logger.info(f"使用代理: {socks_proxies['http']}")
                 
                 logger.info(f"获取网页信息 - 尝试次数: \033[1;94m{email_retry_count + 1}\033[0m.")
-                resp1 = session.get(url=url1, headers=headers, verify=False)
+                resp1 = session.get(url=url2, headers=headers, verify=False)
                 headers1 = resp.headers
                 content1 = resp.text
                 csrftoken = re.findall(r"csrftoken=(\w+);", headers1.get("set-cookie"))[0]
