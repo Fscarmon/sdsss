6d44bbb6127c05c07e8b441b02ed7130add53895
Fscarmon <156925521+Fscarmon@users.noreply.github.com>
2025-02-05 11:08:46 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -111,7 +111,7 @@ def register_email(email):
             url_create_account = "https://www.serv00.com/offer/create_new_account.json"
             header_create_account = {  # 替换成你浏览器复制的header
                 "Host": "www.serv00.com",
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
                 "Accept": "*/*",
                 "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
                 # "Accept-Encoding": "gzip, deflate, br",  # 移除 Accept-Encoding
