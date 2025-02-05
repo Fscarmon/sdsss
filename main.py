34bd5b140ba18423a465c81567497891b11bf8cc
Fscarmon <156925521+Fscarmon@users.noreply.github.com>
2025-02-05 10:42:50 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ def background_task():
 def register_email(email):
     ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
 
-    # 1. 从 https://www.serv00.com 获取 cookie
+    # 1. 获取 cookie
     url_base = "https://www.serv00.com"
     header_base = {
         "User-Agent": ua,
@@ -95,9 +95,6 @@ def register_email(email):
                 raise Exception(f"获取基础页面失败，状态码: {resp_base.status_code}")
 
             headers_base = resp_base.headers
-            content_base = resp_base.text
-
-            # 获取完整的 Cookie 字符串
             cookie = headers_base.get("set-cookie")
             logger.info(f"获取Cookie: {cookie}")
 
@@ -105,7 +102,7 @@ def register_email(email):
                 usernames = get_user_name()
             except Exception as e:
                 logger.error(f"获取用户名失败: {e}")
-                return  # 如果获取用户名失败，直接返回
+                return
 
             _ = usernames.pop()
             first_name = _["name"]
@@ -113,44 +110,9 @@ def register_email(email):
             username = generate_random_username().lower()
             logger.info(f"{email} {first_name} {last_name} {username}")
 
-            # 2. 使用获取的 cookie 访问 url1 和 url3
-            url1 = "https://www.serv00.com/offer/create_new_account"
-
-            header1 = {
-                "Host": "www.serv00.com",
-                "User-Agent": ua,
-                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-                "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
-                "Connection": "keep-alive",
-                "Referer": "https://www.serv00.com/offer",
-                "Sec-GPC": "1",
-                "Upgrade-Insecure-Requests": "1",
-                "Sec-Fetch-Dest": "document",
-                "Sec-Fetch-Mode": "navigate",
-                "Sec-Fetch-Site": "none",
-                "Sec-Fetch-User": "?1",
-                "Priority": "u=1",
-                "Cookie": cookie  # 使用获取的 cookie
-            }
-            captcha_url = "https://www.serv00.com/captcha/image/{}/"
-            header2 = {
-                "Host": "www.serv00.com",
-                "User-Agent": ua,
-                "Accept": "image/avif,image/webp,*/*",
-                "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
-                "Sec-GPC": "1",
-                "Connection": "keep-alive",
-                "Referer": "https://www.serv00.com/offer/create_new_account",
-                "Cookie": cookie,  # 使用获取的 cookie
-                "Sec-Fetch-Dest": "image",
-                "Sec-Fetch-Mode": "no-cors",
-                "Sec-Fetch-Site": "same-origin",
-                "Priority": "u=4",
-                "TE": "trailers",
-            }
-
-            url3 = "https://www.serv00.com/offer/create_new_account.json"
-            header3 = {
+            # 2. 获取 captcha_0
+            url_create_account = "https://www.serv00.com/offer/create_new_account.json"
+            header_create_account = {
                 "Host": "www.serv00.com",
                 "User-Agent": ua,
                 "Accept": "*/*",
@@ -167,42 +129,62 @@ def register_email(email):
                 "Priority": "u=1",
             }
 
-            logger.info(f"请求URL: {url1}")  # 记录请求URL
-            resp = session.get(url=url1, headers=header1, impersonate="chrome124")
-            logger.info(f"获取网页信息状态码: {resp.status_code}")  # 记录状态码
-            if resp.status_code != 200:
-                logger.error(f"获取网页信息失败，状态码: {resp.status_code}, 响应内容: {resp.text}")
-                raise Exception(f"获取网页信息失败，状态码: {resp.status_code}")
+            logger.info(f"请求URL: {url_create_account}")
+            resp_create_account = session.get(url_create_account, headers=header_create_account,
+                                              impersonate="chrome124")
+            logger.info(f"获取 captcha_0 状态码: {resp_create_account.status_code}")
+            if resp_create_account.status_code != 200:
+                logger.error(
+                    f"获取 captcha_0 失败，状态码: {resp_create_account.status_code}, 响应内容: {resp_create_account.text}")
+                raise Exception(f"获取 captcha_0 失败，状态码: {resp_create_account.status_code}")
 
-            # headers = resp.headers  #  不再需要从这里获取， 已经从 url_base 获取
-            content = resp.text
+            content_create_account = resp_create_account.json()
+            logger.info(f"获取 captcha_0 响应: {content_create_account}")
 
             try:
-                captcha_0 = re.findall(r'id=\"id_captcha_0\" name=\"captcha_0\" value=\"(\w+)\">', content)[0]
-                logger.info(f"提取captcha_0成功: {captcha_0}")  # 记录提取的captcha_0
-            except IndexError:
-                logger.error(f"提取captcha_0失败，content: {content}")
+                captcha_0 = content_create_account["__captcha_key"]
+                logger.info(f"提取captcha_0成功: {captcha_0}")
+            except (KeyError, IndexError):
+                logger.error(f"提取captcha_0失败，content: {content_create_account}")
                 raise Exception("提取captcha_0失败")
+
+            # 3. 构建验证码图片URL
+            captcha_url = f"https://www.serv00.com/captcha/image/{captcha_0}/"
+            logger.info(f"验证码图片URL: {captcha_url}")
+
+            header_captcha = {
+                "Host": "www.serv00.com",
+                "User-Agent": ua,
+                "Accept": "image/avif,image/webp,*/*",
+                "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+                "Sec-GPC": "1",
+                "Connection": "keep-alive",
+                "Referer": "https://www.serv00.com/offer/create_new_account",
+                "Cookie": cookie,  # 使用获取的 cookie
+                "Sec-Fetch-Dest": "image",
+                "Sec-Fetch-Mode": "no-cors",
+                "Sec-Fetch-Site": "same-origin",
+                "Priority": "u=4",
+                "TE": "trailers",
+            }
 
             for retry in range(5):
                 time.sleep(random.uniform(0.5, 1.2))
                 logger.info(f"第 {retry + 1} 次尝试获取验证码")
                 try:
-                    captcha_url_formatted = captcha_url.format(captcha_0)
-                    logger.info(f"请求验证码图片URL: {captcha_url_formatted}")  # 记录验证码图片URL
-                    resp = session.get(url=captcha_url_formatted,
-                                         headers=header2, impersonate="chrome124")  # header2 直接使用 cookie
+                    logger.info(f"请求验证码图片URL: {captcha_url}")
+                    resp_captcha = session.get(captcha_url, headers=header_captcha, impersonate="chrome124")
 
-                    logger.info(f"获取验证码图片状态码: {resp.status_code}")  # 记录状态码
-                    if resp.status_code != 200:
-                        logger.error(f"获取验证码图片失败，状态码: {resp.status_code}, 响应内容: {resp.text}")
-                        raise Exception(f"获取验证码图片失败，状态码: {resp.status_code}")
+                    logger.info(f"获取验证码图片状态码: {resp_captcha.status_code}")
+                    if resp_captcha.status_code != 200:
+                        logger.error(f"获取验证码图片失败，状态码: {resp_captcha.status_code}, 响应内容: {resp_captcha.text}")
+                        raise Exception(f"获取验证码图片失败，状态码: {resp_captcha.status_code}")
 
-                    content = resp.content
+                    content_captcha = resp_captcha.content
                     with open("static/image.jpg", "wb") as f:
-                        f.write(content)
+                        f.write(content_captcha)
 
-                    captcha_1 = ocr.classification(content).lower()
+                    captcha_1 = ocr.classification(content_captcha).lower()
                     logger.info(f"OCR识别结果: {captcha_1}")
 
                     if not bool(re.match(r'^[a-zA-Z0-9]{4}$', captcha_1)):
@@ -211,31 +193,46 @@ def register_email(email):
 
                     logger.info(f"识别的验证码: {captcha_1}")
 
-                    data = f"csrfmiddlewaretoken=&first_name={first_name}&last_name={last_name}&username={username}&email={quote(email)}&captcha_0={captcha_0}&captcha_1={captcha_1}&question=0&tos=on"
-                    logger.info(f"POST 数据: {data}")  # 记录POST数据
-                    time.sleep(random.uniform(0.5, 1.2))
-                    logger.info("请求信息")
-                    logger.info(f"请求URL: {url3}")  # 记录请求URL
-                    resp = session.post(url=url3, headers=header3,  # header3 直接使用 cookie
-                                        data=data, impersonate="chrome124")
+                    # 4. 构建并提交表单数据
+                    url_submit = "https://www.serv00.com/offer/create_new_account.json"
+                    header_submit = {
+                        "Host": "www.serv00.com",
+                        "User-Agent": ua,
+                        "Accept": "*/*",
+                        "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+                        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                        "X-Requested-With": "XMLHttpRequest",
+                        "Origin": "https://www.serv00.com",
+                        "Connection": "keep-alive",
+                        "Referer": "https://www.serv00.com/offer/create_new_account",
+                        "Cookie": cookie,
+                        "Sec-Fetch-Dest": "empty",
+                        "Sec-Fetch-Mode": "cors",
+                        "Sec-Fetch-Site": "same-origin",
+                        "Priority": "u=1",
+                    }
+                    data = f"first_name={first_name}&last_name={last_name}&username={username}&email={quote(email)}&captcha_0={captcha_0}&captcha_1={captcha_1}&question=0&tos=on"
+                    logger.info(f"POST 数据: {data}")
 
-                    logger.info(f"提交注册信息状态码: {resp.status_code}")  # 记录状态码
-                    if resp.status_code != 200:
-                        logger.error(f"提交注册信息失败，状态码: {resp.status_code}, 响应内容: {resp.text}")
-                        raise Exception(f"提交注册信息失败，状态码: {resp.status_code}")
+                    logger.info(f"请求URL: {url_submit}")
+                    resp_submit = session.post(url_submit, headers=header_submit, data=data,
+                                                impersonate="chrome124")
 
-                    print(resp.text)
-                    content = resp.json()
-                    logger.info(f"提交注册信息响应: {content}")
+                    logger.info(f"提交注册信息状态码: {resp_submit.status_code}")
+                    if resp_submit.status_code != 200:
+                        logger.error(f"提交注册信息失败，状态码: {resp_submit.status_code}, 响应内容: {resp_submit.text}")
+                        raise Exception(f"提交注册信息失败，状态码: {resp_submit.status_code}")
 
-                    if content.get("captcha") and content["captcha"][0] == "Invalid CAPTCHA":
-                        captcha_0 = content["__captcha_key"]
+                    content_submit = resp_submit.json()
+                    logger.info(f"提交注册信息响应: {content_submit}")
+
+                    if content_submit.get("captcha") and content_submit["captcha"][0] == "Invalid CAPTCHA":
                         logger.warning("验证码错误，正在重新获取")
                         time.sleep(random.uniform(0.5, 1.2))
                         continue
                     else:
                         logger.info(f"邮箱 {email} 注册成功!")
-                        return  # 注册成功，直接退出该邮箱的循环
+                        return
 
                 except Exception as e:
                     logger.error(f"获取验证码或提交注册信息失败: {e}")
@@ -244,7 +241,7 @@ def register_email(email):
             os.remove("static/image.jpg")
 
     except Exception as e:
-        logger.error(f"获取 cookie 失败: {e}")
+        logger.error(f"获取 cookie 或 captcha_0 失败: {e}")
         return
 
 
